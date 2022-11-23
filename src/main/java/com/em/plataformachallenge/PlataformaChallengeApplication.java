00a17442bd3e5d1e5dbbaa370fd56c250a4d18dd
Ezequiel Morales <ezequiel.morales@lamercantil.com.ar>
2022-11-23 18:18:20 -0300

    Creacion proyecto

---
+++ src/main/java/com/em/plataformachallenge/PlataformaChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.em.plataformachallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlataformaChallengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlataformaChallengeApplication.class, args);
+    }
+
+}
